9a2baed89945d67b8ee7b92f902000a9889eb1a3
unknown <bhaviktank5@gmail.com>
2024-01-10 17:24:16 +0530

    mobile optimize

--- index.js
+++ index.js
@@ -38,16 +38,16 @@ gsap.to(".imgweb", {
   //   duration: 2,
 });
 
-tl.from(
-  ".section1 .content .innerContent h1,.section1 .content .innerContent h3,.section1 .content .innerContent p,.mainCard .card",
-  {
-    y: 50,
-    stagger: 0.3,
-    opacity: 0,
-    delay: 0,
-    duration: 1,
-  }
-);
+// tl.from(
+//   ".section1 .content .innerContent h1,.section1 .content .innerContent h3,.section1 .content .innerContent p,.mainCard .card",
+//   {
+//     y: 50,
+//     stagger: 0.3,
+//     opacity: 0,
+//     delay: 0,
+//     duration: 1,
+//   }
+// );
 const section1 = document.querySelector(".section1");
 gsap.to(section1, {
   background: "#101442",
@@ -56,20 +56,21 @@ gsap.to(section1, {
     trigger: ".section1",
     start: "top center",
     end: "top center",
-    scrub: 2,
+    scrub: 1,
   },
 });
-// tl.to(".mainCard .card", {
-//   scrollTrigger: {
-//     trigger: ".section1",
-//     markers: true,
-//     start: "top 10%",
-//     end: "bottom 10%",
-//     scrub: 1,
-//   },
-//   y: 0,
-//   opacity: 0,
-//   //   scale: 1.1,
-//   delay: 0.2,
-//   duration: 2,
-// });
+tl.from(
+  ".section1 .content .innerContent h1,.section1 .content .innerContent h3,.section1 .content .innerContent p,.mainCard .card",
+  {
+    scrollTrigger: {
+      trigger: ".hero_left button",
+      start: "top center",
+      end: "top center",
+      scrub: 1,
+    },
+    y: 50,
+    stagger: 0.4,
+    opacity: 0,
+    duration: 1,
+  }
+);
